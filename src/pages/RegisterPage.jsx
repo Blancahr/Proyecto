2c614dc23ccc021560f500eb4575bcc247e9e4ae
Blancahr <143891402+Blancahr@users.noreply.github.com>
2024-11-10 12:22:46 -0600

    Update RegisterPage.jsx

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,78 +1,76 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useForm } from '../hook/useForm';
 
+export const RegisterPage = () => {
+	const navigate = useNavigate();
 
-import { useForm } from "react-hook-form";
-import { edadValidator } from "./validators";
+	const { name, email, password, onInputChange, onResetForm } =
+		useForm({
+			name: '',
+			email: '',
+			password: '',
+		});
 
-const Formulario = () => {
+	const onRegister = e => {
+		e.preventDefault();
 
-    const { register, formState: { errors }, watch, handleSubmit } = useForm({
-        defaultValues: {
-            nombre: 'Luis',
-            direccion: 'Calle Gran Vía'
-        }
-    });
+		navigate('/dashboard', {
+			replace: true,
+			state: {
+				logged: true,
+				name,
+			},
+		});
 
-    const onSubmit = (data) => {
-        console.log(data);
-    }
+		onResetForm();
+	};
 
-    const incluirTelefono = watch('incluirTelefono');
+	return (
+		<div className='wrapper'>
+			<form onSubmit={onRegister}>
+				<h1>Registrarse</h1>
 
-    return <div>
-        <h2>Formulario</h2>
-        <p>Nombre: {watch('nombre')}</p>
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-                <label>Nombre</label>
-                <input type="text" {...register('nombre', {
-                    required: true,
-                    maxLength: 10
-                })} />
-                {errors.nombre?.type === 'required' && <p>El campo nombre es requerido</p>}
-                {errors.nombre?.type === 'maxLength' && <p>El campo nombre debe tener menos de 10 caracteres</p>}
-            </div>
-            <div>
-                <label>Dirección</label>
-                <input type="text" {...register('direccion', {
-                    required: true
-                })} />
-            </div>
-            <div>
-                <label>Email</label>
-                <input type="text" {...register('email', {
-                    pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i
-                })} />
-                {errors.email?.type === 'pattern' && <p>El formato del email es incorrecto</p>}
-            </div>
-            <div>
-                <label>Edad</label>
-                <input type="text" {...register('edad', {
-                    validate: edadValidator
-                })} />
-                {errors.edad && <p>La edad debe estar entre 18 y 65</p>}
-            </div>
-            <div>
-                <label>País</label>
-                <select {...register('pais')}>
-                    <option value="es">España</option>
-                    <option value="it">Italia</option>
-                    <option value="fr">Francia</option>
-                </select>
-            </div>
-            <div>
-                <label>¿Incluir teléfono?</label>
-                <input type="checkbox" {...register('incluirTelefono')} />
-            </div>
-            {incluirTelefono && (
-                <div>
-                    <label>Teléfono</label>
-                    <input type="text" {...register('telefono')} />
-                </div>
-            )}
-            <input type="submit" value="Enviar" />
-        </form>
-    </div>
-}
+				<div className='input-group'>
+					<input
+						type='text'
+						name='name'
+						id='name'
+						value={name}
+						onChange={onInputChange}
+						required
+						autoComplete='off'
+					/>
+					<label htmlFor='name'>Nombre:</label>
+				</div>
 
+				<div className='input-group'>
+					<input
+						type='email'
+						name='email'
+						id='email'
+						value={email}
+						onChange={onInputChange}
+						required
+						autoComplete='off'
+					/>
+					<label htmlFor='email'>Email:</label>
+				</div>
+				<div className='input-group'>
+					<input
+						type='password'
+						name='password'
+						id='password'
+						value={password}
+						onChange={onInputChange}
+						required
+						autoComplete='off'
+					/>
+					<label htmlFor='password'>Contraseña:</label>
+				</div>
+
+				<button>Registrarse</button>
+			</form>
+		</div>
+	);
+};
